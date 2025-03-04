6de6212ba3180c62a9546ba349ca5a0ba3a4cd45
cfigmart <cfigmart@unicauca.edu.co>
2025-03-04 15:44:46 -0500

    Updated Icon, and added colombian spanish language

--- app/src/main/java/edu/unicauca/aplimovil/mindfulness/MainActivity.kt
+++ app/src/main/java/edu/unicauca/aplimovil/mindfulness/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import edu.unicauca.aplimovil.mindfulness.ui.theme.MindFulnessTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +26,40 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MindFulnessTheme {
-
+                SearchBar()
             }
         }
     }
 }
 
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier
+){
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = stringResource(R.string.placeholder_search)
+            )
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedContainerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        placeholder = {
+            Text(text = stringResource(R.string.placeholder_search))
+        },
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
+    )
+}
+
+@Preview
+@Composable
+fun PreviewSearchBar(){
+    SearchBar()
+}
