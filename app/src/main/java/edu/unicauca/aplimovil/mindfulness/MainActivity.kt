e4b30959b41fa8b7aab0da27238dbfcff97d5ded
cfigmart <cfigmart@unicauca.edu.co>
2025-03-04 15:55:59 -0500

    Updated Icon, and added colombian spanish language

--- app/src/main/java/edu/unicauca/aplimovil/mindfulness/MainActivity.kt
+++ app/src/main/java/edu/unicauca/aplimovil/mindfulness/MainActivity.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -14,8 +19,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import edu.unicauca.aplimovil.mindfulness.ui.theme.MindFulnessTheme
@@ -62,4 +73,36 @@ fun SearchBar(
 @Composable
 fun PreviewSearchBar(){
     SearchBar()
+}
+
+@Composable
+fun AlignYourBodyElement(
+    modifier: Modifier = Modifier
+){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ab1_inversions),
+            contentDescription = stringResource(R.string.ab1_inversions),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(R.string.ab1_inversions),
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = modifier.paddingFromBaseline(top=24.dp, bottom = 8.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun AlingYourBodyElementPreview(){
+    MindFulnessTheme {
+        AlignYourBodyElement()
+    }
 }
