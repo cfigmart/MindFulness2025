d5207cefcbff49227eb3735ced83b3cece57ab21
cfigmart <cfigmart@unicauca.edu.co>
2025-03-06 15:53:59 -0500

    HomeScreen created

--- app/src/main/java/edu/unicauca/aplimovil/mindfulness/MainActivity.kt
+++ app/src/main/java/edu/unicauca/aplimovil/mindfulness/MainActivity.kt
@@ -7,14 +7,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -28,11 +37,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import edu.unicauca.aplimovil.mindfulness.ui.theme.MindFulnessTheme
@@ -43,9 +50,32 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MindFulnessTheme {
-                SearchBar()
+                HomeScreen()
             }
         }
+    }
+}
+
+@Composable
+fun HomeScreen(){
+    Column {
+        Spacer(Modifier.height(32.dp))
+        SearchBar(Modifier.padding(horizontal = 16.dp))
+        HomeSection(title = R.string.alignYourBody) {
+            AlingYourBodyRow()
+        }
+        HomeSection(title = R.string.favorite_collections) {
+            FavoriteCollectionsGrid()
+        }
+        Spacer(Modifier.height(16.dp))
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview(){
+    MindFulnessTheme {
+        HomeScreen()
     }
 }
 
@@ -160,3 +190,105 @@ fun PreviewFavoriteCollectionCard(){
         )
     }
 }
+
+@Composable
+fun AlingYourBodyRow(
+    modifier: Modifier = Modifier
+){
+    LazyRow (
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ){
+        items(alignYourBodyData){ item->
+            AlignYourBodyElement(item.imageId, item.textId)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AlingYourBodyRowPreview(){
+    MindFulnessTheme {
+        AlingYourBodyRow()
+    }
+}
+
+@Composable
+fun FavoriteCollectionsGrid(
+
+    modifier: Modifier = Modifier
+) {
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier.height(168.dp)
+    ) {
+        items(favoriteCollectionsData) { item ->
+            FavoriteCollectionCard(item.imageId, item.textId)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun FavoriteCollectionGridPreview(){
+    MindFulnessTheme {
+        FavoriteCollectionsGrid()
+    }
+}
+
+@Composable
+fun HomeSection(
+    @StringRes title: Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+){
+    Column(modifier) {
+        Text(
+            text = stringResource(title),
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier
+                .paddingFromBaseline(top = 40.dp, bottom = 16.dp)
+                .padding(horizontal = 16.dp)
+        )
+        content()
+    }
+
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun HomeSectionPreview() {
+    MindFulnessTheme{
+        HomeSection(R.string.alignYourBody) {
+            AlingYourBodyRow()
+        }
+    }
+}
+
+private val alignYourBodyData = listOf(
+    R.drawable.ab1_inversions to R.string.ab1_inversions,
+    R.drawable.ab2_quick_yoga to R.string.ab1_quick_yoga,
+    R.drawable.ab4_tabata to R.string.ab4_tabata,
+    R.drawable.ab3_stretching to R.string.ab3_stretching,
+    R.drawable.ab5_hiit to R.string.ab5_hiit,
+    R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
+).map { DrawableResourcePair(it.first, it.second) }
+
+private val favoriteCollectionsData = listOf(
+    R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
+    R.drawable.fc2_nature_meditations to R.string.fc4_nature_meditations,
+    R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
+    R.drawable.fc4_self_massage to R.string.fc4_self_massage,
+    R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
+    R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
+).map { DrawableResourcePair(it.first, it.second) }
+
+
+private data class DrawableResourcePair(
+    @DrawableRes val imageId: Int,
+    @StringRes val textId: Int
+)
